72477d7c201d16cc601f06f4e6e2df0b6a609f99
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-06-08 01:41:34 +0300

    make first letter uppercase

--- src/ini-parser.cc
+++ src/ini-parser.cc
@@ -28,7 +28,7 @@ parser::parser(const std::string &&in_file) {
     if (check_file_extension(in_file) == true)
       this->input_file.open(in_file, std::ios::in);
     else
-      throw std::runtime_error("file should be ended with .ini extension");
+      throw std::runtime_error("File should be ended with .ini extension");
   } catch (std::runtime_error &e) {
     std::cerr << e.what() << std::endl;
   }
